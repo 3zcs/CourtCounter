0ec59b28acfc66f2946e74320cb01b396c55642a
3zcs <abdulaziz.aloshin@gmail.com>
2017-07-17 11:12:28 +0300

    Initial commit

---
+++ app/src/main/java/me/a3zcs/courtcounter/courtcounter/SocreModel.kt
@@ -0,0 +1,10 @@
+package me.a3zcs.courtcounter.courtcounter
+
+import android.arch.lifecycle.ViewModel
+
+/**
+ * Created by root on 17/07/17.
+ */
+
+
+class SocreModel (var teamA:Int = 0,var teamB:Int = 0):ViewModel()
